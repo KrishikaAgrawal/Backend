3f00e05ae12e351b87fafb1fb4d360e63ad21927
KrishikaAgrawal <agrawalkrishika592@gmail.com>
2024-09-18 20:04:06 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // require("dotenv").config({ path: "./env" }); // can give path too
 // but it ruin the consistency of code, require......import......
-
+import "dotenv/config";
 // improved version
-import dotenv from "dotenv"; // but it will not work now, coz need it to config too
+// import dotenv from "dotenv"; // but it will not work now, coz need it to config too
 import connectDB from "./db/index.js"; // sometimes extentions(.js) are also important
 
 // dotenv config
-dotenv.config({
-  path: "./env",
-});
+// dotenv.config({
+//   path: "./env",
+// });
 // as this way is very freshly introduce, it is in experimental version so we need to specify that in in package.json
 connectDB();
