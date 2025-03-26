7724e42f591678b5742be3bd55952ace5bbd646d
KrishikaAgrawal <agrawalkrishika592@gmail.com>
2025-03-27 02:15:04 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -345,5 +345,5 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "User cover image updated successfully"));
 });
 
-
+// EXPORTING ALL 
 export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage };
